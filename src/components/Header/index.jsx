2343447225def7abdd077997d95f4051fac8de00
Allison Silva <allisonsilva1999@gmail.com>
2020-06-24 00:22:58 -0300

    Initial Commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default () => {
+  return (
+    <Container>
+      <h1>Drag and Drop Study</h1>
+    </Container>
+  );
+};
---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0 30px;
+  background-color: #546ac0;
+  color: #fff;
+
+  display: flex;
+  align-items: center;
+`;
